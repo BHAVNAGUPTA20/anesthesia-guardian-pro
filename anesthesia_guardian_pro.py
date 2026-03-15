d4791e463e13e4b37064c9ca511b9a2fd4ab63a2
Dr Bhavna Gupta <72728907+BHAVNAGUPTA20@users.noreply.github.com>
2026-03-15 19:01:27 +0000

    fix google sheet credentials for streamlit cloud

--- anesthesia_guardian_pro.py
+++ anesthesia_guardian_pro.py
@@ -19,9 +19,9 @@ def connect_sheet():
         "https://www.googleapis.com/auth/drive"
     ]
 
-    creds = ServiceAccountCredentials.from_json_keyfile_name(
-        "credentials.json", scope
-    )
+    creds = ServiceAccountCredentials.from_json_keyfile_dict(
+    st.secrets["gcp_service_account"], scope
+)
 
     client = gspread.authorize(creds)
 
